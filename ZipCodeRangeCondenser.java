26d8a1b5cc535ec125dbdce4c6012a65147c65e3
Colin Potter <colinpotter@gmail.com>
2018-08-23 14:09:11 -0700

    Polished code and improved argument handling

--- ZipCodeRangeCondenser.java
+++ ZipCodeRangeCondenser.java
@@ -12,31 +12,71 @@ import java.util.Comparator;
 
 public class ZipCodeRangeCondenser {
 
+    // enable verbose output with this flag
     private static boolean debug = false;
 
-    // program entry point
+    /**
+     * The main method processes the arguments and instantiates the
+     * that specify a range of zip codes. The conversion and validation take place in the getBracketedRange() method.
+     * Valid arguments:
+     * -d turns on the debug flag and prints verbose output
+     * -test runs the unit tests
+     * -i <filename> will read in zip code ranges from the file
+     */
     public static void main (String[] args) {
 
+        for (String arg : args) {
+            if (arg.equals("-d")) {
+                debug = true;
+                break;
+            }
+        }
+
+        // instantiate the condenser
         ZipCodeRangeCondenser condenser = new ZipCodeRangeCondenser();
-        String argument = args[0];
 
         // run tests if -test argument is passed in
-        if ( argument.equals("-test")) {
-            condenser.runUnitTests();
+        for (String arg : args) {
+            if (arg.equals("-test")) {
+                condenser.runUnitTests();
+                break;
+            }
+        }
 
-        } else {
-            // read in the file and merge the zip code ranges
-            String filename = argument;
-            log("filename=" + filename);
-            ArrayList<int[]>  inputRanges = condenser.readRangesFromFile(filename);
-            ArrayList<int[]> outputRanges = condenser.mergeInputRanges(inputRanges);
-            condenser.printResults(outputRanges);
+        // process file if -i flag is passed in
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            if (arg.equals("-i")) {
+                if ( i >= args.length - 1 ) {
+                    System.out.println( "No filename specified for -i flag");
+                } else {
+                    String filename = args[i + 1];
+                    condenser.readInZipCodesAndMerge(filename);
+                    break;
+                }
+            }
+            break; // only process the first file specified to keep things simple
         }
     }
 
-    public ArrayList<int[]> readRangesFromFile(String filename) {
+    /**
+    * This method takes a filename as an argument, reads in the zip code ranges from the file, and merges them
+    */
 
-        // read lines from the file, process strings into integer pair ranges, then merge ranges
+    public void readInZipCodesAndMerge( String filename ) {
+        log("filename=" + filename);
+        ArrayList<int[]>  inputRanges = readRangesFromFile(filename);
+        ArrayList<int[]> outputRanges = mergeInputRanges(inputRanges);
+        printResults(outputRanges);
+    }
+
+    /**
+    * This method reads lines from a text file and converts each line from [xxxxx,yyyyy] format into integer pairs
+    * that specify a range of zip codes. The conversion and validation take place in the getBracketedRange() method
+    */
+
+    private ArrayList<int[]> readRangesFromFile(String filename) {
+
         Path file = FileSystems.getDefault().getPath(filename);
         Charset charset = Charset.forName("US-ASCII");
         ArrayList<int[]> inputRanges = new ArrayList<int[]>();
@@ -46,7 +86,6 @@ public class ZipCodeRangeCondenser {
             String line = null;
             int lineNumber = 0;
             while ((line = reader.readLine()) != null) {
-                log(line);
                 int[] range = getBracketedRange(line, lineNumber);
                 if ( range != null ) {
                     log("Line " + lineNumber + ": " + range[0] + ":" + range[1]);
@@ -60,7 +99,17 @@ public class ZipCodeRangeCondenser {
         return inputRanges;
     }
 
-    public ArrayList<int[]> mergeInputRanges(ArrayList<int[]> inputRanges) {
+    /**
+     * This method takes an ArrayList of int[2] pairs called inputRanges, each of which specify a range of zip codes.
+     * It moves the first inputRange pair into an ArrayList called outputRanges.
+     * Then each subsequent inputRange is compared to each of the outputRanges to see if there are any intersections.
+     * If there are no intersections, then the inputRange is simply moved to the outputRange set.
+     * If the input range intersects with one or more outputRanges, they are all merged together into a new outputRange entry,
+     *   and the originals are removed.
+     * The resulting outputRange set is the minimum number of ranges required to represent the same restrictions as the input
+     */
+
+    private ArrayList<int[]> mergeInputRanges(ArrayList<int[]> inputRanges) {
 
         // merge input ranges and place into output ranges
         ArrayList<int[]> outputRanges = new ArrayList<int[]>();
@@ -83,15 +132,15 @@ public class ZipCodeRangeCondenser {
             ArrayList<Integer> intersectionIndices = new ArrayList<Integer>();
             // compare current input range to each of the current output ranges to determine whether they intersect
             for ( int j = 0; j < outputRanges.size(); j++) {
-                log("j=" + j);
+                log("  j=" + j);
                 int[] outputRange = (int[])outputRanges.get(j);
                 int outputMin = outputRange[0];
                 int outputMax = outputRange[1];
                 // determine whether ranges intersect
                 if ( inputMin > outputMax || inputMax < outputMin ) {
-                    log( inputMin + ":" + inputMax + " and " + outputMin + ":" + outputMax + " do not intersect");
+                    log( "    " + inputMin + ":" + inputMax + " and " + outputMin + ":" + outputMax + " do not intersect");
                 } else {
-                    log( inputMin + ":" + inputMax + " and " + outputMin + ":" + outputMax + " intersect");
+                    log( "    " + inputMin + ":" + inputMax + " and " + outputMin + ":" + outputMax + " intersect");
                     // add the index of the output range to the list that will be merged after visiting each output range
                     intersectionIndices.add(j);
                 }
@@ -147,7 +196,11 @@ public class ZipCodeRangeCondenser {
         return outputRanges;
     }
 
-    public void printResults(ArrayList<int[]> outputRanges) {
+    /**
+    * This method outputs the final outputRanges to standard out in the format [xxxxx,yyyyy], one range per line
+    */
+
+    private void printResults(ArrayList<int[]> outputRanges) {
 
         // print the results
         if ( outputRanges == null ) {
@@ -161,8 +214,14 @@ public class ZipCodeRangeCondenser {
         }
     }
 
-    // convert a line of text into an integer pair range, return null if there is a problem with the line
-    public static int[] getBracketedRange(String input, int lineNumber) {
+    /**
+    * This method converts a line of text into an integer pair range.
+    * It returns null if there is a problem with the line and the line will be ignored.
+    * It verifies that:
+    *     each zip code is between 00000 and 99999
+    *     the second zip code is not less than the first
+    */
+    private int[] getBracketedRange(String input, int lineNumber) {
 
         int[] range = new int[2];
         int open  = input.indexOf("[");
@@ -214,74 +273,82 @@ public class ZipCodeRangeCondenser {
         return range;
     }
 
+    /**
+    * This method runs the unit tests by writing entries to a file, processing the file and verifying the results.
+    * The file is reused for each test and deleted at the end of the method. A test failure is indicated by an output line
+    *  that begins with FAILED TEST
+    */
+
     public void runUnitTests() {
         String testFilename = "testfile.txt";
 
+        System.out.println( "\n==== Test failures are indicated with a line that begins with FAILED TEST. ====\n");
+
         // verify that zero length input returns zero length output
-        System.out.println("Running test 1");
+        System.out.println("== Running test 1 ==");
         writeFile(testFilename, "");
         ArrayList<int[]> inputRanges  = readRangesFromFile(testFilename);
         ArrayList<int[]> outputRanges = mergeInputRanges(inputRanges);
         if ( ! ( outputRanges.size() == 0 ) ) {
-            System.out.println("Failed test 1");
+            System.out.println("FAILED TEST: 1");
         }
 
         // positive test
-        System.out.println("Running test 2");
+        System.out.println("== Running test 2 ==");
         writeFile(testFilename, "[94133,94133]\n[94200,94299]\n[94600,94699]");
         inputRanges  = readRangesFromFile(testFilename);
         outputRanges = mergeInputRanges(inputRanges);
         if ( ! ( outputRanges.size() == 3 ) )
-            System.out.println("Failed test 2a");
+            System.out.println("FAILED TEST: 2a");
         if ( ! ( Arrays.equals((int[])outputRanges.get(0),
                                new int[] {94133, 94133} ) ) )
-            System.out.println("Failed test 2b");
+            System.out.println("FAILED TEST: 2b");
         if ( ! ( Arrays.equals((int[])outputRanges.get(1),
                                new int[] {94200, 94299} ) ) )
-            System.out.println("Failed test 2c");
+            System.out.println("FAILED TEST: 2c");
         if ( ! ( Arrays.equals((int[])outputRanges.get(2),
                                new int[] {94600, 94699} ) ) )
-            System.out.println("Failed test 2d");
+            System.out.println("FAILED TEST: 2d");
 
         // positive test
-        System.out.println("Running test 3");
+        System.out.println("== Running test 3 ==");
         writeFile(testFilename, "[94133,94133]\n[94200,94299]\n[94226,94399]");
         inputRanges  = readRangesFromFile(testFilename);
         outputRanges = mergeInputRanges(inputRanges);
         if ( ! ( outputRanges.size() == 2 ) )
-            System.out.println("Failed test 3a");
+            System.out.println("FAILED TEST: 3a");
         if ( ! ( Arrays.equals((int[])outputRanges.get(0),
                                new int[] {94133, 94133} ) ) )
-            System.out.println("Failed test 3b");
+            System.out.println("FAILED TEST: 3b");
         if ( ! ( Arrays.equals((int[])outputRanges.get(1),
                                new int[] {94200, 94399} ) ) )
-            System.out.println("Failed test 3c");
+            System.out.println("FAILED TEST: 3c");
 
         // negative test, should not accept the aaaaa entry
-        System.out.println("Running test 4");
+        System.out.println("== Running test 4 ==");
         writeFile(testFilename, "[aaaaa,94133]\n[94200,94299]\n[94226,94399]");
         inputRanges  = readRangesFromFile(testFilename);
         if ( ! ( inputRanges.size() == 2 ) )
-            System.out.println("Failed test 4a");
+            System.out.println("FAILED TEST: 4a");
         if ( ! ( Arrays.equals((int[])inputRanges.get(0),
-                               new int[] {94200, 94133} ) ) )
-            System.out.println("Failed test 4b");
+                               new int[] {94200, 94299} ) ) ) 
+            System.out.println("FAILED TEST: 4b");
         if ( ! ( Arrays.equals((int[])inputRanges.get(1),
                                new int[] {94226, 94399} ) ) )
-            System.out.println("Failed test 4c");
+            System.out.println("FAILED TEST: 4c");
 
         // negative test, should not accept the -11111 entry
-        System.out.println("Running test 5");
+        System.out.println("== Running test 5 ==");
         writeFile(testFilename, "[-11111,94133]\n[94200,94299]\n[94226,94399]");
         inputRanges  = readRangesFromFile(testFilename);
         if ( ! ( inputRanges.size() == 2 ) )
-            System.out.println("Failed test 5a");
+            System.out.println("FAILED TEST: 5a");
         if ( ! ( Arrays.equals((int[])inputRanges.get(0),
-                               new int[] {94200, 94133} ) ) )
-            System.out.println("Failed test 5b");
+                               new int[] {94200, 94299} ) ) )
+            System.out.println("FAILED TEST: 5b");
         if ( ! ( Arrays.equals((int[])inputRanges.get(1),
                                new int[] {94226, 94399} ) ) )
-            System.out.println("Failed test 5c");
+            System.out.println("FAILED TEST: 5c");
 
         try {
             Path file = FileSystems.getDefault().getPath(testFilename);
@@ -290,6 +357,10 @@ public class ZipCodeRangeCondenser {
             System.out.println("Failed to delete file: " + testFilename );
         }
     }
+
+    /**
+    * This is a method used by runUnitTests to write zip code pairs to a file so that the file can be read in and processed
+    */
 
     private void writeFile(String filename, String content) {
         Path file = FileSystems.getDefault().getPath(filename);
@@ -301,20 +372,39 @@ public class ZipCodeRangeCondenser {
         }
     }
 
+    /**
+    * This method allows sorting of the zip code output ranges, so that they are more readable when printed to screen
+    */
+
     private class RangeSorter implements Comparator<int[]> {
         public int compare(int[] a, int[] b) {
             return a[0] - b[0];
         }
     }
-    public static void log( String s ) {
+
+    /**
+    * This method writes a string to standard out if debug is enabled
+    */
+
+    private void log( String s ) {
         if ( debug )
             System.out.println(s);
     }
-    public static void log() {
+
+    /**
+    * This method writes a newline to standard out if debug is enabled
+    */
+
+    private void log() {
         if ( debug )
             System.out.println();
     }
-    public static void log_no_newline( String s ) {
+
+    /**
+    * This method writes a string without a newline to standard out if debug is enabled
+    */
+
+    private void log_no_newline( String s ) {
         if ( debug )
             System.out.print(s);
     }
